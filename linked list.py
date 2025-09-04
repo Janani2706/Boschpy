b15fe10497f2ca466dfc5ff01e55f056a91f38e1
Janani2706 <janani.2002054@srec.ac.in>
2025-09-04 14:04:03 +0530

    Add files via upload

---
+++ linked list.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    def delete(self, data):
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next and current.next.data != data:
+            current = current.next
+        if current.next:
+            current.next = current.next.next
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end="-")
+            current = current.next
+        print("None")
+lst = LinkedList()
+lst.append(10)
+lst.append(20)
+lst.append(30)
+lst.prepend(40)
+lst.display() 
+lst.delete(20)
+lst.display()  
